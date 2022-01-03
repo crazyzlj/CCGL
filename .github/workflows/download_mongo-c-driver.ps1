921944f4bb3f2031f49872036f2402b4dffe8ac9
crazyzlj <crazyzlj@gmail.com>
2022-01-03 19:22:23 +0800

    Build with mongodb

--- .github/workflows/download_mongo-c-driver.ps1
+++ .github/workflows/download_mongo-c-driver.ps1
@@ -11,8 +11,12 @@ Set-Location $mongoCPath
 Write-Host "Downloading mongo-c-driver-$version……"
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url,$zipFile)
-Expand-Archive -Path $zipFile -DestinationPath "$env:SystemDrive\"
+Expand-Archive -Path $zipFile -DestinationPath "$env:SystemDrive"
+Get-ChildItem
 Write-Host "Setting environmetal paths of mongo-c-driver……"
 $env:MONGOC_ROOT = $mongoCPath
 $env:MONGOC_LIB_DIR = "$mongoCPath\bin"
 $env:PATH = "$mongoCPath\bin;" + $env:PATH
+Write-Output "MONGOC_ROOT=$env:MONGOC_ROOT"
+Write-Output "MONGOC_LIB_DIR=$env:MONGOC_LIB_DIR"
+Write-Output "PATH=$env:PATH"
