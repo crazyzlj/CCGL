531262a8654ae1a64908c777281eea6e373fc1f9
crazyzlj <crazyzlj@gmail.com>
2022-01-03 20:22:56 +0800

    Test Actions

--- .github/workflows/download_mongo-c-driver.ps1
+++ .github/workflows/download_mongo-c-driver.ps1
@@ -1,5 +1,4 @@
-﻿$mongoCPath = "$env:SystemDrive\mongo-c-driver"
-$version = "1.16.2"
+﻿param ($mongoCPath = "$env:SystemDrive\mongo-c-driver", $version = "1.16.2")
 $url = "https://raw.githubusercontent.com/crazyzlj/Github_Actions_Precompiled_Packages/release/releases/mongo-c-driver-$version-vs2019x64.zip"
 $zipFile = "$mongoCPath\mongo-c-driver.zip"
 
@@ -11,7 +10,7 @@ Set-Location $mongoCPath
 Write-Host "Downloading mongo-c-driver-$version……"
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url,$zipFile)
-Expand-Archive -Path $zipFile -DestinationPath "$env:SystemDrive"
+Expand-Archive -Path $zipFile -DestinationPath "$mongoCPath"
 Get-ChildItem
 Write-Host "Setting environmetal paths of mongo-c-driver……"
 $env:MONGOC_ROOT = $mongoCPath
